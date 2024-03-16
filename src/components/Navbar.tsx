6717e240298ead30628e4b1dc5e419b078f810d9
Pranjal <happybarnwal007@gmail.com>
2024-03-16 22:56:13 +0530

    navbar title font size change

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
     <div className="text-primary font-poppins font-extralight flex items-center justify-between py-5 px-5 sm:px-20">
-      <div className="text-3xl uppercase">SyncSpace</div>
+      <div className="text-2xl sm:text-3xl uppercase">SyncSpace</div>
       <div className="sm:text-xl text-sm flex sm:space-x-10 space-x-2 items-center">
         <p>about</p>
 
