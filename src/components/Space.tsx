a98f287a7f97a484668d29b17e646c318275ab0f
Pranjal <happybarnwal007@gmail.com>
2024-03-19 00:53:51 +0530

    codemirror added

--- src/components/Space.tsx
+++ src/components/Space.tsx
@@ -6,7 +6,7 @@ import Audio from "./Audio";
 
 export default function Space() {
   return (
-    <div className="main flex flex-col h-full w-full">
+    <div className="main flex flex-col h-full w-full space-y-3">
       <div className="top space-x-7 flex w-full h-[75%]">
         <div className="editor w-[70%]">
           <Editor />
