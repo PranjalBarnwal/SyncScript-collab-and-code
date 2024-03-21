5ccf038254b248a7021a902aaef51ac7404b3b9c
Pranjal <happybarnwal007@gmail.com>
2024-03-22 02:53:10 +0530

    adding hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-// import { HashRouter,Routes,Route } from "react-router-dom";
+import { HashRouter,Routes,Route } from "react-router-dom";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 // import { createBrowserRouter, createHashRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
@@ -10,48 +10,48 @@ import ErrorPage from "./routes/ErrorPage.tsx";
 import Space from "./routes/Space.tsx";
 import NamePopup from "./components/NamePopup.tsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/space",
-        element: <Space />,
-      },
-      {
-        path: "/name",
-        element: <NamePopup />,
-      },
-    ],
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     errorElement: <ErrorPage />,
+//     children: [
+//       {
+//         path: "/",
+//         element: <Home />,
+//       },
+//       {
+//         path: "/space",
+//         element: <Space />,
+//       },
+//       {
+//         path: "/name",
+//         element: <NamePopup />,
+//       },
+//     ],
+//   },
+// ]);
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
-
-// ReactDOM.createRoot(document.getElementById("root")!).render(
+// ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 //   <React.StrictMode>
-    
-//       {/* <RouterProvider router={router} /> */}
-//       <HashRouter>
-//       <Routes>
-//         <Route path="/" element={<App />}>
-//           <Route index element={<Home />} />
-//           <Route path="space" element={<Space />} />
-//           <Route path="name" element={<NamePopup />} />
-//           <Route path="*" element={<ErrorPage />} />
-//         </Route>
-//       </Routes>
-//     </HashRouter>  
+//     <RouterProvider router={router} />
 //   </React.StrictMode>
 // );
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    
+      {/* <RouterProvider router={router} /> */}
+      <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="space" element={<Space />} />
+          <Route path="name" element={<NamePopup />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Route>
+      </Routes>
+    </HashRouter>  
+  </React.StrictMode>
+);
 
