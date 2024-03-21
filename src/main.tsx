a1cc972fda84cf9895a396ed91f43a6d0b0c5b2e
Pranjal <happybarnwal007@gmail.com>
2024-03-22 01:14:45 +0530

    updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,40 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { HashRouter } from "react-router-dom";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HashRouter,Routes,Route } from "react-router-dom";
+import { createBrowserRouter, createHashRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Home from "./routes/Home.tsx";
 import ErrorPage from "./routes/ErrorPage.tsx";
 import Space from "./routes/Space.tsx";
 import NamePopup from "./components/NamePopup.tsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/space",
-        element: <Space />,
-      },
-      {
-        path: "/name",
-        element: <NamePopup />,
-      },
-    ],
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     errorElement: <ErrorPage />,
+//     children: [
+//       {
+//         path: "/",
+//         element: <Home />,
+//       },
+//       {
+//         path: "/space",
+//         element: <Space />,
+//       },
+//       {
+//         path: "/name",
+//         element: <NamePopup />,
+//       },
+//     ],
+//   },
+// ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter>
-      <RouterProvider router={router} />
-    </HashRouter>
+    
+      {/* <RouterProvider router={router} /> */}
+      <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="space" element={<Space />} />
+          <Route path="name" element={<NamePopup />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Route>
+      </Routes>
+    </HashRouter>  
   </React.StrictMode>
 );
+
