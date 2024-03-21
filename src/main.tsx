a72498dc4d682197e6548b008a403300aa0ad5fd
Pranjal <happybarnwal007@gmail.com>
2024-03-22 02:54:19 +0530

    nothing as such

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { HashRouter,Routes,Route } from "react-router-dom";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
 // import { createBrowserRouter, createHashRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Home from "./routes/Home.tsx";
 import ErrorPage from "./routes/ErrorPage.tsx";
 import Space from "./routes/Space.tsx";
-import NamePopup from "./components/NamePopup.tsx";
+// import NamePopup from "./components/NamePopup.tsx";
 
 // const router = createBrowserRouter([
 //   {
@@ -47,7 +47,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
           <Route path="space" element={<Space />} />
-          <Route path="name" element={<NamePopup />} />
+          {/* <Route path="name" element={<NamePopup />} /> */}
           <Route path="*" element={<ErrorPage />} />
         </Route>
       </Routes>
