60b1a4ce598e555762474696399acd92c2c5ff8f
Pramod Pandey <ppandey200203@gmail.com>
2024-03-22 19:50:24 +0530

    editor configuration tools added

--- src/components/Space.tsx
+++ src/components/Space.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Editor from "./Editor";
 import OutputBox from "./OutputBox";
 import Panel from "./Panel";
@@ -5,24 +6,30 @@ import Toolbar from "./Toolbar";
 import Audio from "./Audio";
 
 export default function Space() {
+  const [ editorConfig, setEditorConfig ] = useState({
+    theme: 'nord',
+    lang: 'java',
+  });
+
+
   return (
     <div className="main flex flex-col h-full w-full space-y-3">
       <div className="top space-x-7 flex w-full h-[75%]">
         <div className="editor w-[70%]">
-          <Editor />
+          <Editor editorConfig={ editorConfig } />
         </div>
         <div className="topleft flex flex-col w-[30%] h-[100%]">
           <div className="panel h-[20%]">
             <Panel />
           </div>
           <div className="output h-[80%]">
-            <OutputBox/>
+            <OutputBox />
           </div>
         </div>
       </div>
       <div className="bottom flex space-x-7">
         <div className="toolbar w-[70%]">
-          <Toolbar/>
+          <Toolbar setEditorConfig={ setEditorConfig }/>
         </div>
         <div className="audio w-[30%]">
           <Audio/>
--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,16 +1,18 @@
 import Button from "./Button";
+import Dropdown from "./Dropdown";
 
-export default function Toolbar() {
+export default function Toolbar({ setEditorConfig }) {
 
   function classNames(...classes) {
-    return classes.filter(Boolean).join(' ')
+    return classes.filter(Boolean).join(' ');
   }
 
   return (
     <fieldset className="flex flex-row font-poppins  items-center text-primary h-full w-full border-primary border-[1px] rounded-lg  z-20 px-5 space-x-5">
       <legend className="text-lg px-2 font-extralight">Toolbar</legend>
-      <Button title="Languages" />
-      <Button title="Theme" />
+      <Dropdown options={ ['javascript', 'cpp', 'python', 'java'] } setting={ 'lang' } setEditorConfig={ setEditorConfig } />      
+      <Dropdown options={ ['dracula', 'github', 'onedark', 'nord', 'noctis-lila'] } setting={ 'theme' } setEditorConfig={ setEditorConfig } /> 
+
       <Button title="Share snippet" />
       <Button title="Run Code" />
     </fieldset>
